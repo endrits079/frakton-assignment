829959ff963009dd215e0b8320f008f286b0ebc5
Endrit Shabani <endrits079@gmail.com>
2021-07-04 14:19:57 +0200

    created routes, added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Suspense } from "react";
+import "./App.scss";
+import { Switch, Route } from "react-router-dom";
+import Header from "./Components/Header";
+const LazyAlbums = React.lazy(() => import("./Pages/Albums"));
+const LazySingleAlbum = React.lazy(() => import("./Pages/SingleAlbum"));
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Suspense fallback={<p>Loading</p>}>
+        <Switch>
+          <Route exact path="/" component={LazyAlbums} />
+          <Route exact path="/album/:id" component={LazySingleAlbum} />
+        </Switch>
+      </Suspense>
     </div>
   );
 }
